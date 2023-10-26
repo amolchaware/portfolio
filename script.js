99fae8afe0ee14371adeb2a7a0d0a81f402f630b
amolchaware <46417937+amolchaware@users.noreply.github.com>
2023-10-26 09:01:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+// $('.portfolio-button').mousedown(function(){
+//     timeout = setInterval(function(){
+//         window.scrollBy(0,-1); // May need to be -1 to go down
+//     }, 0); // Play around with this number. May go too fast
+
+//     return false;
+// });
+
+socialMediaClick = (mediaSelect) => {
+    switch (mediaSelect) {
+        case whatsapp:
+            'https://wa.me/<9765848377>'
+            break;
+    
+        default:
+            break;
+    }
+
+}
